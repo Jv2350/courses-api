d0aeecd67b276395c31538d93b067946a56d0304
Jv2350 <tp8375735@gmail.com>
2025-06-27 22:00:25 +0530

    Remove CourseApiApplication class and format Course and CourseInstance model classes

--- course-api-backend/src/main/java/com/jayesh/course_api/model/Course.java
+++ course-api-backend/src/main/java/com/jayesh/course_api/model/Course.java
@@ -1,3 +1,13 @@
+@Entity
 public class Course {
-    
+    @Id @GeneratedValue
+    private Long id;
+    private String title;
+    private String courseId;
+    private String description;
+
+    @ManyToMany
+    private List<Course> prerequisites;
+
+    // Getters & Setters
 }
--- course-api-backend/src/main/java/com/jayesh/course_api/model/CourseInstance.java
+++ course-api-backend/src/main/java/com/jayesh/course_api/model/CourseInstance.java
@@ -1,3 +1,12 @@
+@Entity
 public class CourseInstance {
-    
+    @Id @GeneratedValue
+    private Long id;
+    private int year;
+    private int semester;
+
+    @ManyToOne
+    private Course course;
+
+    // Getters & Setters
 }
